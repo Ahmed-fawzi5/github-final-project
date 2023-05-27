4350fb953b0f2ee5cea6e6df65874878da81461d
Ahmed Kotb <118195041+Ahmed-fawzi5@users.noreply.github.com>
2023-05-27 19:52:34 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: Upkar Lidder (IBM)
    # Additional Authors:
-   # Ahmed.fawzi5
+   # Ahmed-fawzi5
    # Input:
    # p, principal amount
    # t, time period in years
